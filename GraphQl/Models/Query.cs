0b558a842c7c8f9faa708c207c889b4edac59c3f
dell <119abadallah118@gmail.com>
2022-07-17 09:44:05 +0200

    First commit

---
+++ GraphQl/Models/Query.cs
@@ -0,0 +1,21 @@
+﻿using GraphQl.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQl.Models
+{
+    public class Query
+    {
+        private IStudent _student;
+        public Query(IStudent student)
+        {
+            _student = student;
+        }
+
+        public List<Student> GetAllStudents => _student.GetAll();
+
+        public Student GetStudent(int id) => _student.Get(id);
+    }
+}
---
+++ GraphQl/Services/StudentService.cs
@@ -0,0 +1,39 @@
+﻿using GraphQl.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQl.Services
+{
+    public interface IStudent
+    {
+        List<Student> GetAll();
+        Student Get(int id);
+    }
+
+    public class StudentService : IStudent
+    {
+        public Student Get(int id)
+        {
+            List<Student> students = new();
+            for (int i = 0; i < 10; i++)
+            {
+                students.Add(new Student { id = i, name = $"st{i}", age = i });
+            }
+
+            return students.FirstOrDefault(x => x.id == id);
+        }
+
+        public List<Student> GetAll()
+        {
+            List<Student> students = new();
+            for (int i = 0; i < 10; i++)
+            {
+                students.Add(new Student { id = i, name = $"st{i}", age = i });
+            }
+
+            return students;
+        }
+    }
+}
